eef41087770e5e18326b5eafa7fe2631f3a4d643
luke <luke.luhai@gmail.com>
2020-08-03 23:25:56 +0800

    ...

--- OtherCode/Other/mergeExcel.py
+++ OtherCode/Other/mergeExcel.py
@@ -1,15 +1,25 @@
 import pandas as pd
 import numpy as np
 import os
-
-path='E:\\temp\\基础信息\\快配表0615\\快配表0615\\'
+import threading as thd
+import time
+path='D:\\快配表0727\\'
 dframes=[]
+t1=time.time()
+
 
 for i in os.listdir(path):
+    
     print(i)
+    tt1=time.time()
     a=pd.read_excel(path+i,sheet_name=None)
     c=a['GGsmCell']
-    dframes.append(c)
+    tt2=time.time()
     
+    dframes.append(c)
+    print(tt1-tt2)
 result = pd.concat(dframes)
-result.to_csv('E:\\temp\\基础信息\\快配表0615\\GGsmCell.csv')
+result.to_csv('d:\\GGsmCell.csv')
+t2=time.time()
+# print('time(sec):',)
+print(t2-t1)
