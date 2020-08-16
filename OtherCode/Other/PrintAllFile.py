b9c3f8d64c86ed435580ba953b1273461dbd07b9
luke <luke.luhai@gmail.com>
2020-08-16 18:51:55 +0800

    .

--- OtherCode/Other/PrintAllFile.py
+++ OtherCode/Other/PrintAllFile.py
@@ -1,6 +1,8 @@
 import os
+import time
+#print (time.strftime("%Y%m%d%H%M%S", time.localtime()) )
 
-f=open('F:\\1需要打印\\aa.txt','w',encoding='utf-8')
+f=open('F:\\KanKan\\VaCache\\aa'+time.strftime("%Y%m%d%H%M%S", time.localtime())+'.csv','w',encoding='utf-8')
 
 def printFile(path,name):
     if os.path.isfile(path+'\\'+name):
@@ -10,5 +12,5 @@ def printFile(path,name):
         for i in os.listdir(path+'\\'+name):
             printFile(path+'\\'+name,i)
     return
-printFile(r'F:','1需要打印')
+printFile(r'F:\\KanKan\\VaCache\\movie\\','潘多拉')
 f.close()
