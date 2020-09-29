13727d1dca40e091dffac0d6dd10f7e35c73f287
luke <luke.luhai@gmail.com>
2020-09-30 00:19:21 +0800

    .

---
+++ ToolsOfWork/BatchmodificationLTE/Batchmodify_handover_parameter.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy
+import xlrd
+
+dataPath='D:\\DataBase\\FDD\\'
+f = pd.read_excel(dataPath+'CellMeasGroup.xlsx',sheet_name='CellMeasGroup',header=0)
+dictCellMeasGroup=
+print(f)
