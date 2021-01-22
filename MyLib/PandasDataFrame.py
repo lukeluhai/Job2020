b9f910bf59a8eb429faed561429b6ecd1fd74bc8
luke <luke.luhai@gmail.com>
2021-01-22 17:32:19 +0800

    .

--- MyLib/PandasDataFrame.py
+++ MyLib/PandasDataFrame.py
@@ -3,8 +3,7 @@ import numpy
 def groupByStr(df,list1,col):
     df[col]=df.groupby(list1)[col].transform(lambda x:' '.join(x))
     return df
-a=pd.read_csv('e:\\lh\\a.csv',encoding='GBK')
-b=groupByStr(a,['a1','a2'],'cell')
-print(b)
+
+
     
 
